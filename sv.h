b80e6b319605fab97266e8bd5850545be522614f
Kirill <73781919+kiruxa097@users.noreply.github.com>
2026-03-25 12:00:50 +0300

    Create sv.h

---
+++ sv.h
@@ -0,0 +1,20 @@
+#ifndef SV_H
+#define SV_H
+
+float mysin1(float x);
+float mysin2(float x);
+float mysin3(float x);
+
+float mycos1(float x);
+float mycos2(float x);
+float mycos3(float x);
+
+float myexp1(float x);
+float myexp2(float x);
+float myexp3(float x);
+
+float myln1(float x);
+float myln2(float x);
+float myln3(float x);
+
+#endif 
