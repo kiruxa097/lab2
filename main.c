001490fcfe6cee8f27c9c7a1f7b2f50153e32c6f
Kirill <73781919+kiruxa097@users.noreply.github.com>
2026-03-26 01:42:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,101 @@
 #include "sv.h"
 
 int main() {
-	float x = sinf(16000);
-	float x1 = mysin1(16000);
-	float x2 = mysin2(16000);
-	float x3 = mysin3(16000);
-	printf("Result lib: %f\n", x);
-	printf("Result mylib1: %f\n", x1);
-	printf("Result mylib2: %f\n", x2);
-	printf("Result mylib3: %f\n", x3);
-
+	int v; float u;
+	float x1, x2, x3, x4;
+	printf("Choose func: \n 1. sin \n 2. cos \n 3. exp \n 4. ln");
+	printf("\n");
+	printf("Choose: ");
+	scanf_s("%d", &v);
+	switch (v)
+	{
+	case 1:
+		printf("Write x: ");
+		scanf_s("%f", &u);
+		x1 = mysin1(u);
+		x2 = mysin2(u);
+		x3 = mysin3(u);
+		x4 = sinf(u);
+		printf("\n");
+		printf("result sum1: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result sum2: ");
+		printf("%f", x2);
+		printf("\n");
+		printf("result sum3: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result sinf in math.h: ");
+		printf("%f", x4);
+		printf("\n");
+		break;
+	case 2:
+		printf("Write x: ");
+		printf("\n");
+		scanf_s("%f", &u);
+		x1 = mycos1(u);
+		x2 = mycos2(u);
+		x3 = mycos3(u);
+		x4 = cosf(u);
+		printf("\n");
+		printf("result sum1: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result sum2: ");
+		printf("%f", x2);
+		printf("\n");
+		printf("result sum3: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result cosf in math.h: ");
+		printf("%f", x4);
+		printf("\n");
+		break;
+	case 3:
+		printf("Write x: ");
+		printf("\n");
+		scanf_s("%f", &u);
+		x1 = myexp1(u);
+		x2 = myexp2(u);
+		x3 = myexp3(u);
+		x4 = expf(u);
+		printf("\n");
+		printf("result sum1: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result sum2: ");
+		printf("%f", x2);
+		printf("\n");
+		printf("result sum3: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result cosf in math.h: ");
+		printf("%f", x4);
+		printf("\n");
+		break;
+	case 4:
+		printf("Write x: ");
+		printf("\n");
+		scanf_s("%f", &u);
+		x1 = myln1(u);
+		x2 = myln2(u);
+		x3 = myln3(u);
+		x4 = logf(u);
+		printf("\n");
+		printf("result sum1: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result sum2: ");
+		printf("%f", x2);
+		printf("\n");
+		printf("result sum3: ");
+		printf("%f", x1);
+		printf("\n");
+		printf("result cosf in math.h: ");
+		printf("%f", x4);
+		printf("\n");
+		break;
+	}
 	return 0;
 }
